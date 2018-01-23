ac8fbc622ea9528ab240c30478cc4a285f4d2055
Paraita Wohler <paraita.wohler@activeeon.com>
2018-01-23 16:58:11 +0100

    Add multiple steps (4)

--- src/main/java/AzureProxyPassThrough.java
+++ src/main/java/AzureProxyPassThrough.java
@@ -4,8 +4,18 @@ import java.io.File;
 import java.io.IOException;
 
 import com.microsoft.azure.management.Azure;
+import com.microsoft.azure.management.network.Network;
+import com.microsoft.azure.management.network.PublicIPAddress;
+import com.microsoft.azure.management.network.TransportProtocol;
+import com.microsoft.azure.management.resources.fluentcore.arm.Region;
 
 public class AzureProxyPassThrough {
+
+    public static void stopIfNeeded(int currentStep, int limitStep) {
+        if (currentStep >= limitStep) {
+            System.exit(0);
+        }
+    }
 
     public static void test1(String credFilePath) {
         File activeeon_creds = new File(credFilePath);
@@ -18,15 +28,50 @@ public class AzureProxyPassThrough {
         System.out.println(azure.subscriptionId());
     }
 
-    public static void test2(int step, String credFilePath) {
+    public static void test2(int limitStep, String credFilePath, String rgGroupName, String regionName) {
         File activeeon_creds = new File(credFilePath);
         Azure azure = null;
+        int currentStep = 0;
         try {
+
             azure = Azure.authenticate(activeeon_creds).withDefaultSubscription();
+            System.out.println("[" + currentStep + "] sessionid:" + azure.subscriptionId());
+            stopIfNeeded(++currentStep, limitStep);
+
+            azure.resourceGroups().define(rgGroupName).withRegion(regionName).create();
+            System.out.println("[" + currentStep + "] created resource group " + rgGroupName);
+            stopIfNeeded(++currentStep, limitStep);
+
+            String networkName = rgGroupName + "Network";
+            Network network = azure.networks().define(networkName).withRegion(regionName)
+                    .withExistingResourceGroup(rgGroupName).withAddressSpace("10.1.0.0/16")
+                    .withSubnet("default", "10.1.0.0/16").create();
+            System.out.println("[" + currentStep + "] created network " + networkName);
+            stopIfNeeded(++currentStep, limitStep);
+
+            String pubIPName = rgGroupName + "PubIP";
+            PublicIPAddress publicIPAddress = azure.publicIPAddresses().define(pubIPName)
+                    .withRegion(regionName).withExistingResourceGroup(rgGroupName).create();
+            System.out.println("[" + currentStep + "] created public ip " + pubIPName);
+            stopIfNeeded(++currentStep, limitStep);
+
+            String LBName = rgGroupName + "LB";
+            azure.loadBalancers().define(LBName).withRegion(regionName)
+                    .withExistingResourceGroup(rgGroupName).defineLoadBalancingRule(LBName)
+                    .withProtocol(TransportProtocol.TCP).fromFrontend(LBName).fromFrontendPort(64738)
+                    .toBackend("pnp").toBackendPort(64738).attach()
+                    .definePublicFrontend(LBName).withExistingPublicIPAddress(publicIPAddress)
+                    .attach().create();
+            System.out.println("[" + currentStep + "] created LB " + LBName);
+            stopIfNeeded(++currentStep, limitStep);
+
+
+
+
+
         } catch (IOException e) {
             e.printStackTrace();
         }
-        System.out.println(azure.subscriptionId());
     }
 
     //    public static void test2() {
@@ -58,6 +103,8 @@ public class AzureProxyPassThrough {
         //String pathParaita = "/home/paraita/Bureau/support/CNES/azure_scaleset_activeeon.creds";
         int step = Integer.valueOf(args[0]);
         String credFilePath = args[1];
-        test2(step, credFilePath);
+        String rgGroupName = args[2];
+        String regionName = args[3];
+        test2(step, credFilePath, rgGroupName, regionName);
     }
 }
